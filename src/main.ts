be16f37b06b51a95960d96a5cf4fb2fdb9d9b29d
Antonio <antonio.dias8997@gmail.com>
2023-04-05 08:35:19 -0300

    Change docs route

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
